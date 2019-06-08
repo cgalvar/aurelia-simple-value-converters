c60aab467517e96dbebcbc44dfe9855ceceb3894
cgalvar <julio_aguila@hotmail.es>
2019-06-08 17:48:32 -0500

    error correction

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,10 @@ import {PLATFORM} from 'aurelia-pal';
 
 export function configure(config: FrameworkConfiguration) {
   config.globalResources([
-    PLATFORM.moduleName('./value-converters/booleanToLetter.ts'),
-    PLATFORM.moduleName('./value-converters/datarow.ts'),
-    PLATFORM.moduleName('./value-converters/date.ts'),
-    PLATFORM.moduleName('./value-converters/direccion.ts'),
-    PLATFORM.moduleName('./value-converters/filelisttoarrayvalueconverter.ts')
+    PLATFORM.moduleName('./value-converters/booleanToLetter'),
+    PLATFORM.moduleName('./value-converters/datarow'),
+    PLATFORM.moduleName('./value-converters/date'),
+    PLATFORM.moduleName('./value-converters/direccion'),
+    PLATFORM.moduleName('./value-converters/filelisttoarrayvalueconverter')
   ]);
 }
